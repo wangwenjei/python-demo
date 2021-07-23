49443ae7f12abed008b600f6de3b761f838f075b
wwjsummer <5208858+wwjsummer@user.noreply.gitee.com>
2021-07-23 17:53:40 +0800

    ALL

--- 函数.py
+++ 函数.py
@@ -1,10 +1,12 @@
 def func(*args, **kwargs):
     print(args, kwargs)
 
-def user(user_name,age):
-    func(user_name,age)
+
+def user(user_name, age):
+    func(user_name, age)
+
 
 func(1, 2, 3, name='a', age='18')
 # (1, 2, 3) {'name': 'a', 'age': '18'}
 
-user('wwj',18)
+user('wwj', 18)
--- 打印进度条.py
+++ 打印进度条.py
@@ -1,10 +1,12 @@
 import time
 
+
 def progress(perent):
     if perent > 1:
         perent = 1
-    res = int(perent*50) * '#'
-    print('\r[%-50s] %d%%' % (res,int(perent*100)), end='')
+    res = int(perent * 50) * '#'
+    print('\r[%-50s] %d%%' % (res, int(perent * 100)), end='')
+
 
 recv_size = 0
 total_size = 102512
