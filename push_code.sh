0f5a3dda3c6ab041da8a1ea6ad8196d6fd603b24
jason <2254964433@qq.com>
2025-07-28 15:16:19 +0800

    2025-07-28

--- push_code.sh
+++ push_code.sh
@@ -1,10 +1,11 @@
 
 commit=`date +%Y-%m-%d`
 
+log="代码更新记录: $commit"
+echo $log >> ./push_code_log.txt
 
 git add .
 git commit -m $commit
-#git push -u origin master
+git push -u origin master
 
-log="代码更新记录: $commit"
-echo $log >> ./push_code_log.txt
+
